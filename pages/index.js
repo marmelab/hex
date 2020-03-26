b64a40d72f8344d3c2a201e01576fae492e58d44
Christophe Lecroq <christophe.lecroq@gmail.com>
2020-03-26 15:58:11 +0100

    Setup Next.js
    - Add gitignore
    - Updates yarn.lock and package.json
    - Create pages directory and settup index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>Hex</title>
+    </Head>
+
+    <main>
+    </main>
+
+    <footer>
+    </footer>
+  </div>
+)
+
+export default Home
