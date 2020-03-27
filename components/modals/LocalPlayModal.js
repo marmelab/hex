ed84d67bad71dd0a6990f97541488c80e69e0a34
Christophe Lecroq <christophe.lecroq@gmail.com>
2020-03-27 11:05:17 +0100

    Basic game launcher
    - Add a button to start a local game
    - Add a modal to configure the game

---
+++ components/modals/LocalPlayModal.js
@@ -0,0 +1,35 @@
+import { Button } from "@chakra-ui/core";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure
+} from "@chakra-ui/core";
+
+import LocalPlayForm from "../forms/LocalPlayForm";
+
+export function LocalPlayModal() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen} variantColor="teal" size="lg">
+        Play (local)
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>2 players on the same device</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <LocalPlayForm />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
