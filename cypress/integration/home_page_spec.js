678c38e1b20cd7cb9537b79466ae1c8740d4cf81
Christophe Lecroq <christophe.lecroq@gmail.com>
2020-03-27 12:01:35 +0100

    Bootstrapping of Cypress
    - Installation of the package
    - Basic test suite for the homepage

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,14 @@
+describe("The Home Page", function() {
+  it("successfully loads", function() {
+    cy.visit("/");
+  });
+
+  it("Launch a local game", function() {
+    cy.contains('Play (local)').click()
+    cy.get('#modal-1-body').invoke('show')
+    cy.contains('2 players on the same device')
+    cy.get('#size')
+      .select('7')
+    cy.contains('Start')
+  });
+});
