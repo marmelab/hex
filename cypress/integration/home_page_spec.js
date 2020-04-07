34091098faee00f32ae4d375a4711d25e706c5f5
Christophe Lecroq <christophe.lecroq@gmail.com>
2020-04-07 17:02:16 +0200

    Add test on menu

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,11 +1,15 @@
-describe("The Home Page", function() {
-
-  before(function() {
+describe("The Home Page", function () {
+  before(function () {
     cy.visit("/");
     cy.contains("Play").click();
   });
 
-  it("Display the '2 players on the same device' modal", function() {
+  it("should display the menu", function () {
+    cy.get('button[name="local-game"]').contains("Play local game");
+    cy.get('button[name="resume-local-game"]').contains("Resume local game");
+  });
+
+  it("Display the '2 players on the same device' modal", function () {
     cy.get("#modal-1-body").invoke("show");
     cy.contains("2 players on the same device");
     cy.get('select[name="size"]').select("7");
