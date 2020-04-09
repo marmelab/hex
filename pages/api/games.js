0da9f9166b5cdb36862991194da9167200684a30
Christophe Lecroq <christophe.lecroq@gmail.com>
2020-04-09 16:26:09 +0200

    Split API default function
    - Should be clearer with switch and different functions

--- pages/api/games.js
+++ pages/api/games.js
@@ -1,26 +1,38 @@
 import { getGameRepository } from "../../models/games/gameRepository";
 
 export default (req, res) => {
-  if (req.method === "POST") {
-    const { player1_nickname, grid } = req.body;
+  const method = req.method;
 
-    getGameRepository()
-      .create({
-        player1_nickname,
-        grid,
-      })
-      .then((game) => {
-        res.status(200).json(game);
-      });
-  } else if (req.method === "GET") {
-    getGameRepository()
-      .findAll({
-        where: {
-          player2_nickname: null,
-        },
-      })
-      .then((game) => {
-        res.status(200).json(game);
-      });
+  switch (method) {
+    case "POST":
+      return post(req, res);
+
+    case "GET":
+      return get(res);
   }
 };
+
+function get(res) {
+  getGameRepository()
+    .findAll({
+      where: {
+        player2_nickname: null,
+      },
+    })
+    .then((game) => {
+      return res.status(200).json(game);
+    });
+}
+
+function post(req, res) {
+  const { player1_nickname, grid } = req.body;
+
+  getGameRepository()
+    .create({
+      player1_nickname,
+      grid,
+    })
+    .then((game) => {
+      return res.status(200).json(game);
+    });
+}
