d71dd30024d2f9018e365d6473c84e4e75c51505
Christophe Lecroq <christophe.lecroq@gmail.com>
2020-04-20 15:52:25 +0200

    Fix playing move on index 0

--- pages/api/games/[uuid].js
+++ pages/api/games/[uuid].js
@@ -16,31 +16,25 @@ export default (req, res) => {
   }
 };
 
-function get(req, res) {
+const get = async (req, res) => {
   const {
     query: { uuid },
   } = req;
 
-  getGameRepository()
-    .findOne({
-      where: {
-        uuid: uuid,
-      },
-    })
-    .then((game) => {
-      const grid = JSON.parse(game.grid);
+  try {
+    const game = await getGameRepository().findOne({ where: { uuid: uuid } });
+    const grid = JSON.parse(game.grid);
 
-      game.dataValues.player = getCurrentPlayer(grid, game.winner);
-      game.dataValues.grid = grid;
+    game.dataValues.player = getCurrentPlayer(grid, game.winner);
+    game.dataValues.grid = grid;
 
-      res.status(200).json(game.dataValues);
-    })
-    .catch((error) => {
-      res.status(404).json(error);
-    });
-}
+    res.status(200).json(game.dataValues);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+};
 
-function patch(req, res) {
+const patch = (req, res) => {
   const {
     query: { uuid },
     headers: { token },
@@ -53,55 +47,46 @@ function patch(req, res) {
   }
 
   if (token === generateToken(player, uuid)) {
-    if (cellIndex && player) {
+    if (cellIndex >= 0 && player) {
       playMove(player, cellIndex, uuid, res);
     }
     res.status(401);
   }
-}
+};
 
 /**
  * This function will update a game state, save it and return it into a JSON response.
  *
- * @param {integer} player
+ * @param {integer} currentPlayer
  * @param {integer} cellIndex
  * @param {uuid} uuid
  * @param {Object} res
  */
-async function playMove(player, cellIndex, uuid, res) {
+const playMove = async (currentPlayer, cellIndex, uuid, res) => {
   try {
     const game = await getGameByUuid(uuid);
 
     game.grid = JSON.parse(game.grid);
-
-    const updatedGame = applyMoveOnGame(game.dataValues, player, cellIndex);
+    const updatedGame = applyMoveOnGame(
+      game.dataValues,
+      currentPlayer,
+      cellIndex
+    );
     updatedGame.grid = JSON.stringify(updatedGame.grid);
 
-    getGameRepository()
-      .update(
-        {
-          grid: updatedGame.grid,
-          player: updatedGame.player,
-          winner: updatedGame.winner,
-        },
-        {
-          where: {
-            uuid: uuid,
-          },
-        }
-      )
-      .then(async () => {
-        try {
-          updatedGame.grid = JSON.parse(updatedGame.grid);
-          res.status(200).json(updatedGame);
-        } catch (error) {
-          res.status(404).json(error);
-        }
-      });
+    const { grid, player, winner } = updatedGame;
+
+    await getGameRepository().update(
+      { grid, player, winner },
+      { where: { uuid: uuid } }
+    );
+
+    updatedGame.grid = JSON.parse(updatedGame.grid);
+    res.status(200).json(updatedGame);
   } catch (error) {
     res.status(404).json(error);
   }
-}
+};
 
 /**
  *
@@ -109,25 +94,15 @@ async function playMove(player, cellIndex, uuid, res) {
  * @param {string} uuid
  * @param {Object} res
  */
-async function updateSecondPlayerNickname(secondPlayerNickname, uuid, res) {
-  getGameRepository()
-    .update(
+const updateSecondPlayerNickname = async (secondPlayerNickname, uuid, res) => {
+  try {
+    await getGameRepository().update(
       { secondPlayerNickname },
-      {
-        where: {
-          uuid: uuid,
-        },
-      }
-    )
-    .then(async () => {
-      try {
-        const game = await getGameByUuid(uuid);
-        res.status(200).json(game);
-      } catch (error) {
-        res.status(404).json(error);
-      }
-    })
-    .catch((error) => {
-      res.status(400).json(error);
-    });
-}
+      { where: { uuid: uuid } }
+    );
+    const game = await getGameByUuid(uuid);
+    res.status(200).json(game);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
