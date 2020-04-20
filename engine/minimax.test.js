34a9988b01e3a2a53c80ec74cea886dafee62fea
Christophe Lecroq <christophe.lecroq@gmail.com>
2020-04-20 14:11:14 +0200

    Remove constants in tests

--- engine/minimax.test.js
+++ engine/minimax.test.js
@@ -1,20 +1,23 @@
 import {
+  getAdvice,
   getAllPossibleGrids,
   getWinningPathForGrids,
-  getAdvice,
-  getAdviceIndex,
 } from "./minimax";
-import {
-  FIRST_PLAYER_VALUE,
-  SECOND_PLAYER_VALUE,
-  ADVISE_VALUE,
-} from "./player";
+
+/**
+ * Reminder
+ *
+ * NO_PLAYER_VALUE = 0
+ * FIRST_PLAYER_VALUE = 1
+ * SECOND_PLAYER_VALUE = 2
+ * WINNER_LINE_VALUE = 3
+ * ADVISE_VALUE = 4
+ */
 
 describe("Minimax implementation", () => {
-  
   it("should get all grids possible for a grid configuration", () => {
     const grid = [1, 1, 0, 2, 2, 2, 1, 0, 0];
-    const player = FIRST_PLAYER_VALUE;
+    const player = 1;
 
     const allPossibleGrids = getAllPossibleGrids(grid, player);
 
@@ -34,7 +37,7 @@ describe("Minimax implementation", () => {
       { grid: [1, 1, 0, 2, 2, 2, 1, 0, 1], index: 8 },
     ];
 
-    const scores = getWinningPathForGrids(grids, FIRST_PLAYER_VALUE);
+    const scores = getWinningPathForGrids(grids, 1);
     const expectedScores = [
       {
         index: 2,
@@ -59,8 +62,8 @@ describe("Minimax implementation", () => {
   it("should returns the next best move as grid (maximized)", () => {
     const grid = [1, 1, 0, 2, 2, 2, 1, 0, 0];
 
-    const advice = getAdvice(grid, FIRST_PLAYER_VALUE);
-    const expectedAdvice = [1, 1, ADVISE_VALUE, 2, 2, 2, 1, 0, 0];
+    const advice = getAdvice(grid, 1);
+    const expectedAdvice = [1, 1, 4, 2, 2, 2, 1, 0, 0];
 
     expect(advice).toEqual(expectedAdvice);
   });
@@ -68,8 +71,8 @@ describe("Minimax implementation", () => {
   it("should returns the next best move as grid (minimized)", () => {
     const grid = [0, 0, 0, 1, 1, 2, 0, 0, 0];
 
-    const advice = getAdvice(grid, SECOND_PLAYER_VALUE);
-    const expectedAdvice = [0, 0, ADVISE_VALUE, 1, 1, 2, 0, 0, 0];
+    const advice = getAdvice(grid, 2);
+    const expectedAdvice = [0, 0, 4, 1, 1, 2, 0, 0, 0];
 
     expect(advice).toEqual(expectedAdvice);
   });
